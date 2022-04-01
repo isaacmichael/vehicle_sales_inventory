b4a0efd2c5df41127868dcbba6b8242e41040f4b
isaacmichael <54340385+isaacmichael@users.noreply.github.com>
2022-04-01 15:23:48 -0400

    adds routes to model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :vehicle_sales
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  get 'searchMake',to:"vehicle_sales#searchMake"
+  get 'searchModel',to:"vehicle_sales#searchModel"
+  get 'searchYear',to:"vehicle_sales#searchYear"
+  get 'searchColor',to:"vehicle_sales#searchColor"
   # Defines the root path route ("/")
   # root "articles#index"
 end
